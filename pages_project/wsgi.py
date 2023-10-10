d4e3c33005bb940410a4e9ff13199b4a1783e864
sandesh <apparalasandesh@gmail.com>
2023-10-10 16:20:08 +0530

    new vercel

--- pages_project/wsgi.py
+++ pages_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pages_project.settings')
 
 application = get_wsgi_application()
+
+WSGI_APPLICATION = 'vercel_app.wsgi.app'
+
